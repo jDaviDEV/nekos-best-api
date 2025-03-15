e391bab00252478b91f8bfe48de4444511f5e4d6
Juan David Correa Franco <david0804correa@gmail.com>
2025-03-14 20:02:25 -0500

    Creacion de un

--- neko-api/src/App.jsx
+++ neko-api/src/App.jsx
@@ -1,17 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 import { useFetch } from './hooks/useFetch';
+import { useAddToAlbum } from './hooks/useAddToAlbum';
 
 function App() {  
   const url = "https://nekos.best/api/v2/husbando";
   const data = useFetch(url);
 
+  const onButtonClick = () => {
+    
+  }
+  
+
+
   return (
     <>
       <div>
+        <button onClick={onButtonClick}>Randomizar imagen</button>
         {data.isLoading ? (<></>) : (<h1>artist name: {data.results[0].artist_name}</h1>)}
-        {data.isLoading ? (<></>) : (<img src={data.results[0].url}/>)}
+        {data.isLoading ? (<></>) : (<img src={data.results[0].url} width="500px" height="500px"/>)}
       </div>
+
+      <div>
+        <h1>ALMBUM</h1>
+        <button>Añadir img a album</button>
+        <ul>
+          <li>{}</li>
+        </ul>
+      </div>
+
       
     </>
   )
