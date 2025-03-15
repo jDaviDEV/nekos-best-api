36f0f87eedb64b64363ff855d51ce4a658d64580
Juan David Correa Franco <david0804correa@gmail.com>
2025-03-14 19:21:20 -0500

    Creacion de custom hook useFetch y prueba en app

---
+++ neko-api/src/hooks/useFetch.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+export const useFetch = (url="https://nekos.best/api/v2/neko") => {
+    const [data,setData] = useState({
+        isLoading: true
+    });
+
+    const getFetch = async () => {
+        setData({...data});
+        const response = await fetch(url);
+        setData({...await response.json(), isLoading: false})
+    }
+
+    useEffect(() => {
+      getFetch();
+    
+      return () => {
+      }
+    }, [url])
+    
+  return (data)
+}
